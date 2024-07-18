ae07667fff3cf424d7651a2b1e57f9343faf3ec4
Deepika Teegapuram <dteegapuram@gmail.com>
2024-07-18 15:13:29 -0600

    added dialog custom component to show result.

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,29 +1,48 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import ResultModal from "./ResultModal";
+
+/*
+let timer; 
+this timer instace will be shared accross 
+all the instances of this component.
+instead of that, use refs 
+*/
 
 export default function TimerChallenge({title, targetTime}){
+    const timer = useRef(); 
     const [timerExpired, setTimerExpired] = useState(false);
     const [timerStarted, setTimerStarted] = useState(false);
 
+
     function handleStart(){
-        setTimeout(() => {
+        timer.current = setTimeout(() => {
           setTimerExpired(true);
         }, targetTime * 1000);
         setTimerStarted(true);
     }
 
-    return (<section className="challenge">
+    function handleStop(){
+         clearTimeout(timer.current);
+    }
+
+    return (
+    <>
+    {timerExpired && <ResultModal targetTime={targetTime} result="lost"/>}
+    <section className="challenge">
       <h2>{title}</h2>
       {timerExpired && <p>You Lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime >1 ? 's': ''}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
             {timerStarted ? 'Stop': 'Start'} Challenge
         </button>
       </p>
       <p className={timerStarted ? 'active' : undefined}>
          {timerStarted ? 'Time is running...': 'Timer inactive'}
       </p>
-    </section>);
+    </section>
+    </>
+    );
 }
