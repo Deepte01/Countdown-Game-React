80695bd02cc91e495b765b227d5acaf7d470e386
Deepika Teegapuram <dteegapuram@gmail.com>
2024-07-18 23:51:24 -0600

    used timeout intervals to check the remaining time.

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -11,19 +11,22 @@ instead of that, use refs
 export default function TimerChallenge({title, targetTime}){
     const timer = useRef(); 
     const dialog = useRef();
-    const [timerExpired, setTimerExpired] = useState(false);
-    const [timerStarted, setTimerStarted] = useState(false);
-
+    const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+    //const [timerStarted, setTimerStarted] = useState(false);
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime *1000;
+    if(timeRemaining <= 0) {
+      clearInterval(timer.current);
+      setTimeRemaining(targetTime * 1000);
+      dialog.current.open();
+    }
     function handleStart(){
-        timer.current = setTimeout(() => {
-          setTimerExpired(true);
-          dialog.current.open();
-        }, targetTime * 1000);
-        setTimerStarted(true);
+        timer.current = setInterval(() => {
+          setTimeRemaining(prevTime => prevTime -10);
+        }, 10);
     }
 
     function handleStop(){
-         clearTimeout(timer.current);
+        clearInterval(timer.current);
     }
 
     return (
@@ -36,12 +39,12 @@ export default function TimerChallenge({title, targetTime}){
         {targetTime} second{targetTime >1 ? 's': ''}
       </p>
       <p>
-        <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? 'Stop': 'Start'} Challenge
+        <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? 'Stop': 'Start'} Challenge
         </button>
       </p>
-      <p className={timerStarted ? 'active' : undefined}>
-         {timerStarted ? 'Time is running...': 'Timer inactive'}
+      <p className={timerIsActive ? 'active' : undefined}>
+         {timerIsActive ? 'Time is running...': 'Timer inactive'}
       </p>
     </section>
     </>
