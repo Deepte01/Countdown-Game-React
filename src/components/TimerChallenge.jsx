8c7c0d749cd8d4d464d3bfb2e9a57a5b48660cf0
Deepika Teegapuram <dteegapuram@gmail.com>
2024-07-19 00:02:08 -0600

    display the results and reset timer.

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -16,8 +16,10 @@ export default function TimerChallenge({title, targetTime}){
     const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime *1000;
     if(timeRemaining <= 0) {
       clearInterval(timer.current);
-      setTimeRemaining(targetTime * 1000);
       dialog.current.open();
+    }
+    function handleReset() {
+      setTimeRemaining(targetTime * 1000);
     }
     function handleStart(){
         timer.current = setInterval(() => {
@@ -31,10 +33,14 @@ export default function TimerChallenge({title, targetTime}){
 
     return (
     <>
-    <ResultModal ref={dialog} targetTime={targetTime} result="lost"/>
+    <ResultModal 
+      ref={dialog} 
+      targetTime={targetTime} 
+      remainingTime={timeRemaining}
+      onReset={handleReset}/>
     <section className="challenge">
       <h2>{title}</h2>
-      {timerExpired && <p>You Lost!</p>}
+      {!timerIsActive && <p>You Lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime >1 ? 's': ''}
       </p>
